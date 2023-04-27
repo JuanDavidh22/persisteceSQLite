9a4130bbf6354277ecb91af4e38ddd48b433300c
Andresssg <andres-guevara1@upc.edu.co>
2023-04-26 22:32:42 -0500

    Add DB helper

---
+++ app/src/main/java/com/edu/unipiloto/sqlite_lab/BeerDatabaseHelper.java
@@ -0,0 +1,90 @@
+package com.edu.unipiloto.sqlite_lab;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BeerDatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "beerdb";
+    private static final int DB_VERSION = 1;
+    public static final String TABLE_NAME = "BEERS";
+    public static final String COL_ID = "id";
+    public static final String COL_BRAND = "BRAND";
+    public static final String COL_COLOR = "COLOR";
+    public static final String COL_SCORE = "SCORE";
+    public static final String COL_COUNTRY = "COUNTRY";
+    public static final String COL_PRICE = "PRICE";
+
+
+    public BeerDatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = String.format("CREATE TABLE BEERS(" +
+                        "%s INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        "%s TEXT," +
+                        "%s TEXT," +
+                        "%s INTEGER," +
+                        "%s TEXT," +
+                        "%s INTEGER);", COL_ID, COL_BRAND,
+                COL_COLOR, COL_SCORE, COL_COUNTRY, COL_PRICE);
+        db.execSQL(query);
+        insertBeer(db, "Poker", "Rubia", 4, "Colombia", 3000);
+        insertBeer(db, "Aguila", "Rubia", 4, "Colombia", 3000);
+        insertBeer(db, "Club Colombia", "Roja", 4, "Colombia", 3700);
+    }
+
+    public boolean insertBeer(SQLiteDatabase db, String brand, String color, int score, String country, int price) {
+        ContentValues beerValues = new ContentValues();
+        beerValues.put(COL_BRAND, brand);
+        beerValues.put(COL_COLOR, color);
+        beerValues.put(COL_SCORE, score);
+        beerValues.put(COL_COUNTRY, country);
+        beerValues.put(COL_PRICE, price);
+        long result = db.insert(TABLE_NAME, null, beerValues);
+
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void initData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        onUpgrade(db, 1, 1);
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Cursor result = db.rawQuery(query, null);
+        return result;
+    }
+
+    public Cursor getData(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = String.format("SELECT * FROM " + TABLE_NAME + " WHERE %s=" + id + "", COL_ID);
+        Cursor result = db.rawQuery(query, null);
+        return result;
+    }
+
+    public Cursor findDataColor(String color) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = String.format("SELECT * FROM " + TABLE_NAME + " WHERE %s='" + color + "'", COL_COLOR);
+        Cursor result = db.rawQuery(query, null);
+        return result;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String query = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(query);
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/edu/unipiloto/sqlite_lab/MainActivity.java
+++ app/src/main/java/com/edu/unipiloto/sqlite_lab/MainActivity.java
@@ -2,7 +2,10 @@ package com.edu.unipiloto.sqlite_lab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BeerDatabaseHelper beerDatabaseHelper = new BeerDatabaseHelper(this);
+        try {
+            SQLiteDatabase db = beerDatabaseHelper.getReadableDatabase();
+        }catch (SQLException e){
+            Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(this, "Database available", Toast.LENGTH_SHORT).show();
     }
 }
